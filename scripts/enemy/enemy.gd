3bd92fabdb965d460c92a72aab5251232d4d7c0e
behrtic <thatfancyzygarde@gmail.com>
2026-03-05 15:46:08 -0500

    Fixed Enemy Movement, Enemies now spawn off-cam

--- scripts/enemy/enemy.gd
+++ scripts/enemy/enemy.gd
@@ -1,15 +1,18 @@
 extends CharacterBody2D
 
 #Basic Enemy Movement Script
-var speed = 80
-var player
+@export var speed: float = 120.0
+var player: Node2D
+
 
 func _physics_process(delta):
 	
 	if player == null:
 		return
 		
-	var direction = (player.global_position - global_position).normalized()
+	var direction = player.global_position - global_position
+	direction = direction.normalized()
+	
 	velocity = direction * speed
 	
 	move_and_slide()
--- scripts/systems/enemy_spawner.gd
+++ scripts/systems/enemy_spawner.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var enemy_scene : PackedScene
 @export var spawn_radius = 500
 @export var spawn_rate = 2.0
+@export var arena_size: Vector2 = Vector2(1200, 800)
 
 var timer = 0
 var player
@@ -15,7 +16,7 @@ func _ready():
 		push_error("Player not found!")
 
 func _process(delta):
-	
+	print(player)
 	timer += delta
 	
 	if timer >= spawn_rate:
@@ -23,13 +24,22 @@ func _process(delta):
 		spawn_enemy()
 		
 func spawn_enemy():
+		var enemy = enemy_scene.instantiate()
 		
-	var enemy = enemy_scene.instantiate()
+		var side = randi() % 4
+		var spawn_pos = Vector2()
+	
+		enemy.position = spawn_pos
+		enemy.player = player
 		
-	var angle = randf() * TAU
-	var pos = Vector2(cos(angle), sin(angle)) * spawn_radius
-		
-	enemy.position = player.position + pos
-	enemy.player = player
-		
-	add_child(enemy)
+		if side == 0:
+			spawn_pos = Vector2(-50, randf_range(0, arena_size.y)) # left
+		elif side == 1:
+			spawn_pos = Vector2(arena_size.x + 50, randf_range(0, arena_size.y))
+		elif side == 2:
+			spawn_pos = Vector2(randf_range(0, arena_size.x), -50)
+		else:
+			spawn_pos = Vector2(randf_range(0, arena_size.x), arena_size.y + 50)
+			
+		enemy.position = spawn_pos
+		add_child(enemy)
